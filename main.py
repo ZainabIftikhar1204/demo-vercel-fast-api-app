89e8d008067c75fe8626631416d41aa547018dca
Zainab Iftikhar <70856406+ZainabIftikhar1204@users.noreply.github.com>
2024-07-31 20:54:32 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-from fastapi import FastAPI
-import nest_asyncio
-from pyngrok import ngrok
-import uvicorn
-import requests
-import torch
+from time import time
+from fastapi import FastAPI, __version__
+# from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
+# app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get('/')
-async def index():
- return {'message':'Hello,World'}
+# html = f"""
+# <!DOCTYPE html>
+# <html>
+#     <head>
+#         <title>FastAPI on Vercel</title>
+#         <link rel="icon" href="/static/favicon.ico" type="image/x-icon" />
+#     </head>
+#     <body>
+#         <div class="bg-gray-200 p-4 rounded-lg shadow-lg">
+#             <h1>Hello from FastAPI@{__version__}</h1>
+#             <ul>
+#                 <li><a href="/docs">/docs</a></li>
+#                 <li><a href="/redoc">/redoc</a></li>
+#             </ul>
+#             <p>Powered by <a href="https://vercel.com" target="_blank">Vercel</a></p>
+#         </div>
+#     </body>
+# </html>
+# """
 
-@app.get('/{name}')
-async def get_name(name: str):
- return {'Welcome To Here': f'{name}'}
+@app.get("/")
+async def root():
+    return HTMLResponse(html)
+
+@app.get('/ping')
+async def hello():
+    return {'res': 'pong', 'version': __version__, "time": time()}
